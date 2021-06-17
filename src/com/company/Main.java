b6f6d6272c2fee858ad9a038acea30711bfbf94d
Niranjan <niranjany@testvagrant.com>
2021-06-17 12:15:11 +0530

    Created template for operation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,13 @@ public class Main {
     int choice;
     Scanner in = new Scanner(System.in);
 
-	System.out.println("Basic Calculator \nSelect Operation to perform\n1:Add\n2:Sub\n3:Multiply\n4:Divide\n=>");
+	System.out.println("Basic Calculator \nSelect Operation to perform\n1:Add\n2:Sub\n3:Multiply\n4:Divide");
 	choice = in.nextInt();
 	switch(choice) {
-        case 1: System.out.println("1 Selected");break;
+        case 1: System.out.println("Addition Operation");break;
+        case 2: System.out.println("Subtraction operation");break;
+        case 3: System.out.println("Multiplication operation");break;
+        case 4: System.out.println("Division operation");break;
 
         }
     }
