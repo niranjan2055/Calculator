d124820687583252453afde7d86d5f59cded597b
Niranjan <niranjany@testvagrant.com>
2021-06-17 12:13:16 +0530

    created base for the program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("I am working");
+    int choice;
+    Scanner in = new Scanner(System.in);
+
+	System.out.println("Basic Calculator \nSelect Operation to perform\n1:Add\n2:Sub\n3:Multiply\n4:Divide\n=>");
+	choice = in.nextInt();
+	switch(choice) {
+        case 1: System.out.println("1 Selected");break;
+
+        }
     }
 }
